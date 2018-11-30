46c1a2bb522163dcce38e2fc375c5c2b8d55c61c
liurenyuan <renyuan.liu@netbraintech.com>
2018-11-30 10:17:07 +0800

    delete 2.0.2 spring boot release

--- knowledge-cloud-parent/nb-kc-api/src/main/java/com/netbrain/kc/api/service/PermissionService.java
+++
@@ -1,9 +0,0 @@
-package com.netbrain.kc.api.service;
-
-import java.util.List;
-
-import com.netbrain.kc.api.model.datamodel.PermissionEntity;
-
-public interface PermissionService {
-    List<PermissionEntity> findAllById(List<String> permissionIds);
-}
--- knowledge-cloud-parent/nb-kc-framework/src/main/java/com/netbrain/kc/framework/model/constant/Constants.java
+++
@@ -1,5 +0,0 @@
-package com.netbrain.kc.framework.model.constant;
-
-public class Constants {
-	public static final String TOKEN="token";
-}
